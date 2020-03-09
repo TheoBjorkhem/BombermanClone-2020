3847890bc6cea50f39c44caa746d85d9239d745d
TheoBjorkhem <55138821+TheoBjorkhem@users.noreply.github.com>
2020-03-09 10:04:28 +0100

    k

---
+++ Assets/TileGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject tile;
+
+    [SerializeField]
+    protected int width = 5;
+
+    [SerializeField]
+    protected int height = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*float offsetX = width / 2f - 0.5f;
+        float offsetY = height / 2f - 0.5f;*/
+
+        Vector3 offset = new Vector2(width / 2f - 0.5f, height / 2f - 0.5f);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                PlaceTile(offset, x, y);
+            }
+        }
+    }
+
+    protected virtual void PlaceTile(Vector3 offset, int x, int y)
+    {
+        GameObject newTile = Instantiate(tile, new Vector3(x, y, transform.position.z) - offset, Quaternion.identity);
+
+        newTile.name = "(" + x + "," + y + ")";
+
+        newTile.transform.parent = this.transform;
+    }
+
+}
---
+++ Assets/WallGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : TileGenerator
+{
+
+    protected override void PlaceTile(Vector3 offset, int x, int y)
+    {
+        if (x == 0 || y == 0)
+        {
+            base.PlaceTile(offset, x, y);
+        }
+        else if (x == width - 1 || y == height - 1)
+        {
+            base.PlaceTile(offset, x, y);
+        }
+        else if (x % 2 == 0 && y % 2 == 0)
+        {
+            base.PlaceTile(offset, x, y);
+        }
+
+    }
+
+}
